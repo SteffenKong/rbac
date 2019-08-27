bc184794ccd812da07b4e0884b55b8a991204e6f
konghy <konghy@pvc123.com>
2019-08-27 08:23:35 +0800

    fix修改sql脚本

--- design/rbac.sql
+++ design/rbac.sql
@@ -12,8 +12,8 @@ create table if not exists monda_admin(
     phone varchar(191) not null comment '手机',
     status tinyint default 1 comment '状态 0 - 禁用 1 - 启用',
     role_id mediumint unsigned comment '角色id 角色id为0代表是超级管理员',
-    created_at int not null comment '创建时间',
-    updated_at int not null comment '修改时间',
+    created_at datetime  comment '创建时间',
+    updated_at datetime  comment '修改时间',
     primary key (id),
     unique key uk_account(account),
     unique key uk_nick_name(nick_name),
@@ -23,7 +23,7 @@ create table if not exists monda_admin(
     index idx_status (status)
 )charset=utf8,engine=innodb;
 
-INSERT INTO `monda_admin`(account,password,nick_name,email,phone,status,role_id,created_at,updated_at) VALUES ('SteffenKong','$2y$10$CKYcOw..M0vQwnEurUi8heyzCFhpinOgYG2NmV/K5LbBx5taVXPRi','孔浩源','3266023724@qq.com','15622903410',1,0,unix_timestamp,unix_timestamp);
+INSERT INTO `monda_admin`(account,password,nick_name,email,phone,status,role_id) VALUES ('SteffenKong','$2y$10$CKYcOw..M0vQwnEurUi8heyzCFhpinOgYG2NmV/K5LbBx5taVXPRi','孔浩源','3266023724@qq.com','15622903410',1,0);
 
 
 
@@ -31,7 +31,7 @@ INSERT INTO `monda_admin`(account,password,nick_name,email,phone,status,role_id,
 create table if not exists monda_roles(
     id mediumint unsigned not null auto_increment,
     role_name varchar(191) not null comment '角色名称',
-    description text comment '角色描述'
+    description text comment '角色描述',
     status tinyint default 1 comment '角色状态 0 - 禁用  1 - 启用',
     created_at int not null comment '创建时间',
     updated_at int not null comment '修改时间',
@@ -45,12 +45,12 @@ create table if not exists monda_roles(
 create table if not exists monda_permission_role(
     id mediumint unsigned not null auto_increment,
     role_id mediumint unsigned not null comment '角色id',
-    permission_id mediumint unsigned not null commnet '权限id',
+    permission_id mediumint unsigned not null comment '权限id',
     created_at int not null comment '创建时间',
     updated_at int not null comment '修改时间',
     primary key (id),
     index idx_role_id (role_id),
-    index idx_permission_id (permission_id),
+    index idx_permission_id (permission_id)
 )charset=utf8,engine=innodb;
 
 
